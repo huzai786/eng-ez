65e7842e62a1595914e5a0da0f855fc0cf043887
muhammad huzaifa <muhammadhuzaifagamer123@gmail.com>
2022-06-08 01:40:36 +0500

    qt layout setup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from qtf import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QFileDialog, QProgressBar, QVBoxLayout
+
+        
 
 
 class Engez(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -9,9 +11,11 @@ class Engez(QtWidgets.QMainWindow, Ui_MainWindow):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
+
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     widget = Engez()
     widget.show()
     sys.exit(app.exec_())
+
--- playground.py
+++ playground.py
@@ -1,8 +1,15 @@
-from fpdf import FPDF
+# from fpdf import FPDF
+import requests
+import pprint
 
+res = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en/vague')
 
-pdf = FPDF('L')
-pdf.add_page()
-pdf.set_font('helvetica', "B", 16)
-pdf.cell(40, 10, 'HELLO WORLD')
-pdf.output('hw.pdf')
+with open('ret.txt', 'w+', encoding='utf-8') as f:
+    f.write(pprint.pformat((res.json())))
+# print(pprint.pformat(res.text))
+# print(len(res.text))
+# pdf = FPDF('L')
+# pdf.add_page()
+# pdf.set_font('helvetica', "B", 16)
+# pdf.cell(40, 10, 'HELLO WORLD')
+# pdf.output('hw.pdf')
